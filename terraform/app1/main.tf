16a00f7df291a5c3ea4e20619fb3d7be7a02e5ae
Robert Wittman <rwittman@confluent.io>
2024-07-10 11:25:10 -0400

    Add default region

--- terraform/app1/main.tf
+++ terraform/app1/main.tf
@@ -17,6 +17,7 @@ provider "kubernetes" {}
 variable "region" {
   type        = string
   description = "The region to deploy this instance to"
+  default = "us-east-1"
 }
 
 resource "kubernetes_namespace" "namespace" {
